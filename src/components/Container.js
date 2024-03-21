5aaaff1407e70895d3857e48e335760d15f3d81b
honeypatkar70 <honeypatkar70@gmail.com>
2024-03-21 15:19:01 +0530

    Adding a new project and chaging project iamges

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,20 +3,20 @@ import React from 'react'
 export default function Container(props) {
     let{name, imageUrl, url, description} = props;
   return (
-    <div class="hover1">
-    <div className="card my-3">
+    <div className="hover1">
+    <div className="card my-3 bg-black text-white">
       <img src={imageUrl} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
-        <a href={url} className="btn btn-dark" target='_blank' rel='noreferrer'>Click Here</a>
+        <a href={url} className="btn btn-outline-light" target='_blank' rel='noreferrer'>Click Here</a>
       </div>
       <style jsx="true">
         {`
         .hover1 div {
           transition : 0.5s ease;
         }
-        .hover1 div:hover{
+        .hover1 img:hover{
           -webkit-transform: scale(0.9);
           -ms-transform: scale(0.9);
           transform: scale(0.9);
---
+++ src/components/Project.js
@@ -0,0 +1,70 @@
+// const counter = "./images/counter.png"
+import image1 from "./images/counter.png"
+import image2 from "./images/resume.png"
+import image3 from "./images/textchange.png"
+import image4 from "./images/tic-tac-toe.png"
+import image5 from "./images/todo.png"
+import image6 from "./images/weather.png"
+import image7 from "./images/moviedb.png"
+import image8 from "./images/imagegallery.png"
+
+const work = [
+  {
+    imageUrl: image1,
+    url: "https://honeypatkar.github.io/Button-Count/",
+    name: "Button Click Counter",
+    description:
+      "This is a practice project made using HTML, CSS, and JavaScript. It's a simple button click counter application.",
+  },
+  {
+    imageUrl: image2,
+    url: "https://honeypatkar.github.io/My-Resume/",
+    name: "Resume",
+    description:
+      "This is my personal resume showcasing my skills, experiences, and projects. It's designed using HTML, CSS, and JavaScript.",
+  },
+  {
+    imageUrl: image3,
+    url: "https://text-change.netlify.app/",
+    name: "TextChange",
+    description:
+      "TextChange is a website where you can perform interesting text transformations. I developed it using React JS during my practice sessions.",
+  },
+  {
+    imageUrl: image4,
+    url: "https://honeypatkar.github.io/Tic-Tac-Toe/",
+    name: "Tic Tac Toe",
+    description:
+      "Tic Tac Toe is a classic game implemented using React JS. It offers an interactive and enjoyable gaming experience.",
+  },
+  {
+    imageUrl: image5,
+    url: "https://honeypatkar.github.io/Todo-App/",
+    name: "Todo-List App",
+    description:
+      "Todo-List App is a React-based application where you can manage your daily tasks efficiently. It supports adding, editing, and deleting tasks.",
+  },
+  {
+    imageUrl: image6,
+    url: "https://honeypatkar.github.io/Weather_App/",
+    name: "Weather App",
+    description:
+      "Weather App is developed using React JS. It allows users to check the weather conditions of any city worldwide, providing accurate and real-time data.",
+  },
+  {
+    imageUrl: image7,
+    url: "https://moviedb-honey.netlify.com",
+    name: "Movie DB",
+    description:
+      "Movie DB is a web application created with Vite + React. It enables users to search for movies and series by name or IMDb ID, providing comprehensive information about each title.",
+  },
+  {
+    imageUrl: image8,
+    url: "https://image-gallery-honey.netlify.com",
+    name: "Image Gallery",
+    description:
+      "Image Gallery is a Vite + React application that allows users to explore a wide range of images based on their preferences. It provides an enjoyable browsing experience for image enthusiasts.",
+  },
+];
+
+export { work };
