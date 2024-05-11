04837a3a3ff09b9aaabed4b35775878327e777fb
honeypatkar <honeypatkar70@gmail.com>
2024-05-11 13:46:43 +0530

    Adding typeWritter Effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
@@ -7,19 +8,40 @@ export default function Home() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Let's Contact {" "}
+              Let's Contact{" "}
               <a href="/" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true"></span> Message Me <span aria-hidden="true">&rarr;</span>
+                <span className="absolute inset-0" aria-hidden="true"></span>{" "}
+                Message Me <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Hii I'm Honey <br/>
-              Frontend Developer
+              Hii I'm Honey <br />
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Front End Developer !")
+                    .pauseFor(2000)
+                    .deleteChars(21)
+                    .typeString("Web Designer !")
+                    .pauseFor(2000)
+                    .deleteChars(14)
+                    .typeString("Programmer !")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .start()
+                }}
+                options={{
+                  loop: true,
+                }}
+              />
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-            I'm a intermidiate Frotend developer and learning web devlopment. I'm looking for job opportunity. This is my Personal Portfolio. Here I have included some of my project that i made during practice. Hopefully you like them.
+              I'm a intermidiate Frotend developer and learning web devlopment.
+              I'm looking for job opportunity. This is my Personal Portfolio.
+              Here I have included some of my project that i made during
+              practice. Hopefully you like them.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -28,7 +50,10 @@ export default function Home() {
               >
                 Get started
               </a>
-              <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+              <a
+                href="/"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
