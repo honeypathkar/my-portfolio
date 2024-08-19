e07c8f415f8cd5d72e1b231fb910924656c580ec
honeypathkar <honeypatkar70@gmail.com>
2024-08-19 15:19:38 +0530

    Changing skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,85 +1,76 @@
-import React, { useState } from "react";
+import React from "react";
 
 const skillsData = [
-  { title: "HTML", percent: "90" },
-  { title: "CSS", percent: "70" },
-  { title: "Tailwind CSS", percent: "50" },
-  { title: "JavaScript", percent: "60" },
-  { title: "Bootstrap", percent: "80" },
-  { title: "React Js", percent: "65" },
-  { title: "Redux Toolkit", percent: "30" },
-  { title: "Git & Github", percent: "90" },
-  { title: "NextJs", percent: "20" },
-  { title: "Node Js", percent: "40" },
+  {
+    title: "HTML",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+  },
+  {
+    title: "CSS",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+  },
+  {
+    title: "Tailwind CSS",
+    image:
+      "https://camo.githubusercontent.com/0568e2de313626b2bd9b96f326941b012d45e9a4db1a23aa78bd8036207e57f8/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667",
+  },
+  {
+    title: "JavaScript",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+  },
+  {
+    title: "Bootstrap",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+  },
+  {
+    title: "React Js",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+  },
+  {
+    title: "Redux Toolkit",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+  },
+  {
+    title: "Git",
+    image:
+      "https://camo.githubusercontent.com/fcafa5ebc1f5f789ae7d012a3ecd8fe7bda49516591caf7c37698f764165d880/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+  },
+  {
+    title: "Node Js",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+  },
 ];
 
 const Skills = () => {
-  const [currentSkill, setCurrentSkill] = useState(skillsData[0]);
-
-  const handleSkillClick = (skill) => {
-    setCurrentSkill(skill);
-  };
-
   return (
-    <>
+    <div className="container">
       <div
         className="mt-32 text-center text-4xl underline"
         style={{ fontFamily: "'Pangolin', cursive" }}
       >
         Skills
       </div>
-      <main
-        className="grid w-full my-10 text-gray-100 place-content-center"
-      >
-        <section className="p-6 space-y-6 border border-black rounded-xl md:grid md:grid-cols-2 md:gap-4 sm:space-y-0">
-          <div className="grid grid-cols-2 gap-4">
-            {skillsData.map((skill) => (
-              <button
-                key={skill.title}
-                onClick={() => handleSkillClick(skill)}
-                className={`px-4 py-2 text-xl text-gray-100 transition bg-[#6FB3B8] rounded-md h-14 w-44 hover:bg-[#388087] ${
-                  currentSkill.title === skill.title
-                    ? "font-bold ring-2 ring-gray-100"
-                    : ""
-                }`}
-              >
-                {skill.title}
-              </button>
-            ))}
+      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 justify-center items-center mt-8">
+        {skillsData.map((item, index) => (
+          <div
+            className="flex flex-col items-center bg-white py-8 rounded-md shadow-md hover:scale-105"
+            key={index}
+          >
+            <img src={item.image} alt={item.title} className="w-16 h-16" />
+            <h1 className="mt-2 text-lg font-medium text-center">
+              {item.title}
+            </h1>
           </div>
-
-          <div className="flex items-center justify-center">
-            <svg className="transform -rotate-90 w-72 h-72">
-              <circle
-                cx="145"
-                cy="145"
-                r="120"
-                stroke="currentColor"
-                strokeWidth="30"
-                fill="transparent"
-                className="text-[#388087]"
-              />
-
-              <circle
-                cx="145"
-                cy="145"
-                r="120"
-                stroke="currentColor"
-                strokeWidth="30"
-                fill="transparent"
-                strokeDasharray={2 * Math.PI * 120}
-                strokeDashoffset={
-                  (2 * Math.PI * 120 * (100 - parseInt(currentSkill.percent))) /
-                  100
-                }
-                className="text-[#6FB3B8]"
-              />
-            </svg>
-            <span className="absolute text-5xl text-black">{`${currentSkill.percent}%`}</span>
-          </div>
-        </section>
-      </main>
-    </>
+        ))}
+      </div>
+    </div>
   );
 };
 
