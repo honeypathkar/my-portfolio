0e7330deb3256df2ffc1de744fa57b7f34cfd170
honeypathkar <honeypatkar70@gmail.com>
2024-08-20 15:15:54 +0530

    Adding more skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,21 @@
 import React from "react";
 
-const skillsData = [
+//Css framework skills
+const cssFramwork = [
+  {
+    title: "Tailwind CSS",
+    image:
+      "https://camo.githubusercontent.com/0568e2de313626b2bd9b96f326941b012d45e9a4db1a23aa78bd8036207e57f8/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667",
+  },
+  {
+    title: "Bootstrap",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+  },
+];
+
+//Frontend Development Skills
+const frontEndSkills = [
   {
     title: "HTML",
     image:
@@ -12,25 +27,37 @@ const skillsData = [
       "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
   },
   {
-    title: "Tailwind CSS",
-    image:
-      "https://camo.githubusercontent.com/0568e2de313626b2bd9b96f326941b012d45e9a4db1a23aa78bd8036207e57f8/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667",
-  },
-  {
     title: "JavaScript",
     image:
       "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-  },
-  {
-    title: "Bootstrap",
-    image:
-      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
   },
   {
     title: "React Js",
     image:
       "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
   },
+];
+
+//Backend Developmet Skills
+const backendSkills = [
+  {
+    title: "Node Js",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+  },
+  {
+    title: "Express Js",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXgAuAwfMMDkwZScsLkBAeZ4GjyfB7GtIzcA&s",
+  },
+  {
+    title: "MongoDB",
+    image:
+      "https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_wordmark_logo_icon_146425.png",
+  },
+];
+
+const otherSkills = [
   {
     title: "Redux Toolkit",
     image:
@@ -42,9 +69,18 @@ const skillsData = [
       "https://camo.githubusercontent.com/fcafa5ebc1f5f789ae7d012a3ecd8fe7bda49516591caf7c37698f764165d880/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
   },
   {
-    title: "Node Js",
+    title: "React Native",
+    image: "https://reactnative.dev/img/header_logo.svg",
+  },
+  {
+    title: "Github",
     image:
-      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+      "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+  },
+  {
+    title: "Vs Code",
+    image:
+      "https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj",
   },
 ];
 
@@ -57,18 +93,73 @@ const Skills = () => {
       >
         Skills
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 justify-center items-center mt-8">
-        {skillsData.map((item, index) => (
-          <div
-            className="flex flex-col items-center bg-white py-8 rounded-md shadow-md hover:scale-105"
-            key={index}
-          >
-            <img src={item.image} alt={item.title} className="w-16 h-16" />
-            <h1 className="mt-2 text-lg font-medium text-center">
-              {item.title}
-            </h1>
+      <div className="mt-10">
+        <div className="mb-3">
+          <h1 className="bold text-xl text-black">Frontend Tools:</h1>
+          <div className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-12 gap-4 justify-center items-center mt-3">
+            {frontEndSkills.map((item, index) => (
+              <div
+                className="flex flex-col items-center bg-white py-6 rounded-md shadow-md hover:scale-105"
+                key={index}
+              >
+                <img src={item.image} alt={item.title} className="w-12 h-12" />
+                <h1 className="mt-2 text-sm font-medium text-center">
+                  {item.title}
+                </h1>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
+        <div className="mb-3">
+          <h1 className="bold text-xl text-black">Css Framework:</h1>
+          <div className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-12 gap-4 justify-center items-center mt-3">
+            {cssFramwork.map((item, index) => (
+              <div
+                className="flex flex-col items-center bg-white py-6 rounded-md shadow-md hover:scale-105"
+                key={index}
+              >
+                <img src={item.image} alt={item.title} className="w-12 h-12" />
+                <h1 className="mt-2 text-sm font-medium text-center">
+                  {item.title}
+                </h1>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="mb-3">
+          <h1 className="bold text-xl text-black">
+            Backend Development Tools:
+          </h1>
+          <div className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-12 gap-4 justify-center items-center mt-3">
+            {backendSkills.map((item, index) => (
+              <div
+                className="flex flex-col items-center bg-white py-6 rounded-md shadow-md hover:scale-105"
+                key={index}
+              >
+                <img src={item.image} alt={item.title} className="w-12 h-12" />
+                <h1 className="mt-2 text-sm font-medium text-center">
+                  {item.title}
+                </h1>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="mb-3">
+          <h1 className="bold text-xl text-black">Other tools and skills:</h1>
+          <div className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-12 gap-4 justify-center items-center mt-3">
+            {otherSkills.map((item, index) => (
+              <div
+                className="flex flex-col items-center bg-white py-6 rounded-md shadow-md hover:scale-105"
+                key={index}
+              >
+                <img src={item.image} alt={item.title} className="w-12 h-12" />
+                <h1 className="mt-2 text-sm font-medium text-center">
+                  {item.title}
+                </h1>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
