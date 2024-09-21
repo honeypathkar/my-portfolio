c3809ea4a9e1169a4f366047fd9dbf3b374b194a
honeypathkar <honeypatkar70@gmail.com>
2024-09-21 13:42:47 +0530

    Adding react pagination

--- src/components/pages/WorkPage.js
+++ src/components/pages/WorkPage.js
@@ -1,28 +1,21 @@
 import React, { useState } from "react";
 import Container from "../Container";
 import { work } from "../utils/data";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ReactPaginate from "react-paginate";
 
 export default function Work() {
   const [currentPage, setCurrentPage] = useState(0);
-  const [page, setPage] = useState(1);
 
   const itemsPerPage = 4;
   const totalPages = Math.ceil(work.length / itemsPerPage);
 
-  const handlePrevious = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
-    setPage(page - 1);
-  };
-
-  const handleNext = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
-    setPage(page + 1);
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
   };
 
   const offset = currentPage * itemsPerPage;
   const currentPageData = work.slice(offset, offset + itemsPerPage);
+
   return (
     <div className="mt-28">
       <div className="container my-4">
@@ -41,24 +34,33 @@ export default function Work() {
             );
           })}
         </div>
+
         <div className="pagination-controls mt-4 flex justify-center space-x-4 mb-12">
-          <button
-            onClick={handlePrevious}
-            disabled={currentPage === 0}
-            className="px-4 py-2 text-white rounded disabled:opacity-50 bg-[#6FB3B8]"
-          >
-            <ArrowBackIosIcon />
-          </button>
-          <span className="p-[10px] text-xl">
-            {page} / {totalPages}
-          </span>
-          <button
-            onClick={handleNext}
-            disabled={currentPage === totalPages - 1}
-            className="px-4 py-2 text-white rounded disabled:opacity-50 bg-[#6FB3B8]"
-          >
-            <ArrowForwardIosIcon />
-          </button>
+          <ReactPaginate
+            previousLabel={"← Previous"}
+            nextLabel={"Next →"}
+            breakLabel={"..."}
+            pageCount={totalPages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={handlePageClick}
+            containerClassName={"flex space-x-4"}
+            activeClassName={"bg-[#6FB3B8] text-white rounded"}
+            pageClassName={
+              "px-4 py-2 text-black border border-black rounded cursor-pointer hover:bg-[#6FB3B8] hover:text-white transition-all"
+            }
+            previousClassName={
+              "px-4 py-2 text-white bg-[#6FB3B8] rounded border border-black disabled:opacity-50 cursor-pointer"
+            }
+            nextClassName={
+              "px-4 py-2 text-white bg-[#6FB3B8] border border-black rounded disabled:opacity-50 cursor-pointer"
+            }
+            disabledClassName={"opacity-50 cursor-not-allowed"}
+            pageLinkClassName={"w-full h-full text-center"}
+            previousLinkClassName={"w-full h-full text-center"}
+            nextLinkClassName={"w-full h-full text-center"}
+            breakClassName={"px-4 py-2 border rounded cursor-pointer"}
+          />
         </div>
       </div>
     </div>
