c8abf92f13ccb52001b5e33263cc7f87a5ddfcc8
honey-rapydlaunch <honey@rapydlaunch.com>
2025-01-29 16:52:08 +0530

    Adding Skeleton Effect

--- src/components/pages/WorkPage.js
+++ src/components/pages/WorkPage.js
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Container from "../Container";
+import SkeletonCard from "../SkeletonCard";
 import ReactPaginate from "react-paginate";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import Loader from "../Loader";
 
 export default function Work() {
-  const [works, setWorks] = useState([]); // State for fetched data
+  const [works, setWorks] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const itemsPerPage = 6; // Number of items per page
+  const itemsPerPage = 6;
   const offset = currentPage * itemsPerPage;
 
-  // Fetch data from the API
   useEffect(() => {
     const fetchWorks = async () => {
       try {
@@ -42,29 +41,12 @@ export default function Work() {
 
   const currentPageData = works.slice(offset, offset + itemsPerPage);
 
-  if (loading) {
-    return (
-      <div className="text-center my-40 text-white">
-        <div className="flex flex-col items-center text-center mb-5 text-white">
-          <h2 className="text-3xl font-bold text-center">Featured Projects</h2>
-          <div className="w-20 h-1 bg-purple-600 mt-3"></div>
-        </div>
-        <div className="mt-20">
-          <Loader /> {/* Show loader once */}
-          <p className="mt-5">Loading...</p>
-        </div>
-      </div>
-    );
-  }
-
   if (error) {
     return (
-      <div className="my-40">
-        <div className="flex flex-col items-center text-center mb-5 text-white">
-          <h2 className="text-3xl font-bold text-center">Featured Projects</h2>
-          <div className="w-20 h-1 bg-purple-600 mt-3"></div>
-        </div>
-        <div className="text-center mt-20 text-red-500">{error}</div>
+      <div className="my-40 text-white text-center">
+        <h2 className="text-3xl font-bold">Featured Projects</h2>
+        <div className="w-20 h-1 bg-purple-600 mt-3 mx-auto"></div>
+        <div className="mt-20 text-red-500">{error}</div>
       </div>
     );
   }
@@ -73,55 +55,57 @@ export default function Work() {
     <div className="mt-28">
       <div className="container my-4">
         <div className="flex flex-col items-center text-center mb-5 text-white">
-          <h2 className="text-3xl font-bold text-center">Featured Projects</h2>
+          <h2 className="text-3xl font-bold">Featured Projects</h2>
           <div className="w-20 h-1 bg-purple-600 mt-3"></div>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
-          {currentPageData.map((element) => (
-            <div
-              className="col-span-1"
-              key={element._id} // Assuming each work has a unique _id
-            >
-              <Container
-                name={element.name}
-                imageUrl={element.image}
-                url={element.url}
-                description={element.description}
-                tools={element.tools}
-                source={element.source}
-              />
-            </div>
-          ))}
+          {loading
+            ? Array(itemsPerPage)
+                .fill(null)
+                .map((_, index) => <SkeletonCard key={index} />)
+            : currentPageData.map((element) => (
+                <Container
+                  key={element._id}
+                  name={element.name}
+                  imageUrl={element.image}
+                  url={element.url}
+                  description={element.description}
+                  tools={element.tools}
+                  source={element.source}
+                />
+              ))}
         </div>
 
-        <div className="pagination-controls mt-4 flex justify-center space-x-4 mb-12">
-          <ReactPaginate
-            previousLabel={<ChevronLeftIcon />}
-            nextLabel={<ChevronRightIcon />}
-            breakLabel={"..."}
-            pageCount={Math.ceil(works.length / itemsPerPage)}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={3}
-            onPageChange={handlePageClick}
-            containerClassName={"flex space-x-4"}
-            activeClassName={"bg-purple-600 text-white rounded"}
-            pageClassName={
-              "px-4 py-2 text-white border-[1px] border-purple-800 rounded cursor-pointer hover:bg-purple-600 hover:text-white transition-all"
-            }
-            previousClassName={
-              "px-4 py-2 text-white bg-purple-600 rounded border-[1px] border-purple-800 disabled:opacity-50 cursor-pointer"
-            }
-            nextClassName={
-              "px-4 py-2 text-white bg-purple-600 border-[1px] border-purple-800 rounded disabled:opacity-50 cursor-pointer"
-            }
-            disabledClassName={"opacity-50 cursor-not-allowed"}
-            pageLinkClassName={"w-full h-full text-center"}
-            previousLinkClassName={"w-full h-full text-center"}
-            nextLinkClassName={"w-full h-full text-center"}
-            breakClassName={"px-4 py-2 border rounded cursor-pointer"}
-          />
-        </div>
+        {!loading && (
+          <div className="pagination-controls mt-4 flex justify-center space-x-4 mb-12">
+            <ReactPaginate
+              previousLabel={<ChevronLeftIcon />}
+              nextLabel={<ChevronRightIcon />}
+              breakLabel={"..."}
+              pageCount={Math.ceil(works.length / itemsPerPage)}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={3}
+              onPageChange={handlePageClick}
+              containerClassName={"flex space-x-4"}
+              activeClassName={"bg-purple-600 text-white rounded"}
+              pageClassName={
+                "px-4 py-2 text-white border-[1px] border-purple-800 rounded cursor-pointer hover:bg-purple-600 hover:text-white transition-all"
+              }
+              previousClassName={
+                "px-4 py-2 text-white bg-purple-600 rounded border-[1px] border-purple-800 disabled:opacity-50 cursor-pointer"
+              }
+              nextClassName={
+                "px-4 py-2 text-white bg-purple-600 border-[1px] border-purple-800 rounded disabled:opacity-50 cursor-pointer"
+              }
+              disabledClassName={"opacity-50 cursor-not-allowed"}
+              pageLinkClassName={"w-full h-full text-center"}
+              previousLinkClassName={"w-full h-full text-center"}
+              nextLinkClassName={"w-full h-full text-center"}
+              breakClassName={"px-4 py-2 border rounded cursor-pointer"}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
