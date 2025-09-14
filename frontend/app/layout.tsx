b32c1ed236bdd8f58bca1457e9ae9660f9a6aaab
honeypathkar <honeypatkar70@gmail.com>
2025-09-14 14:59:50 +0530

    Added favicon and vercel analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 import React from "react";
 import Footer from "../components/Footer";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({
   children,
@@ -15,9 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+       <link rel="shortcut icon" href="/android-chrome-512x512.png" type="image/x-icon" />
+      </head>
       <body>
         <Toaster richColors position="top-right" theme="dark" closeButton />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
