5a8b2c976e0bb48bf6bcea9cfc6b02b3f758beba
honeypathkar <honeypatkar70@gmail.com>
2025-10-26 15:37:44 +0530

    Added view port color and rendering favicon correctly.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,4 @@
-export const metadata = {
-  title: "Honey | Portfolio | MERN Stack Developer | Android Developer | Programmer",
-  description:
-    "I am a MERN Stack developer with a passion for building dynamic, user-focused web and mobile applications. With solid experience in MongoDB, Express.js, React.js, and Node.js, I create full-stack solutions that are scalable, high-performing, and visually appealing. In addition to web development, I work on Android app development using React Native and Flutter, enabling me to craft cross-platform mobile experiences with smooth performance and responsive UI. I have solved 500+ Data Structures and Algorithms problems across various platforms, which has sharpened my logical thinking and coding skills. My skill set also includes HTML, CSS, JavaScript, and modern styling frameworks like Tailwind CSS and Bootstrap, allowing me to turn complex ideas into clean, user-friendly interfaces.",
-};
-
+import type { Metadata, Viewport } from 'next'
 import "./globals.css";
 import React from "react";
 import Footer from "../components/Footer";
@@ -15,7 +10,22 @@ const bricolageGrotesque = Bricolage_Grotesque({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
+
+export const metadata: Metadata = {
+  title:
+    "Honey | Portfolio | MERN Stack Developer | Android Developer | Programmer",
+  description:
+    "I am a MERN Stack developer with a passion for building dynamic, user-focused web and mobile applications. With solid experience in MongoDB, Express.js, React.js, and Node.js. ",
   
+  icons: {
+    shortcut: '/android-chrome-512x512.png',
+  }
+};
+
+export const viewport: Viewport = {
+  themeColor: '#111827cc',
+};
+
 
 export default function RootLayout({
   children,
@@ -24,13 +34,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={bricolageGrotesque.className}>
-      <head>
-        <link
-          rel="shortcut icon"
-          href="/android-chrome-512x512.png"
-          type="image/x-icon"
-        />
-      </head>
       <body>
         <Toaster richColors position="top-right" theme="dark" closeButton />
         {children}
